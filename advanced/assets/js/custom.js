dfa14dd8e12146e4b34ba98c93f7ef55e4edbb32
謝愛佳 <victoriatjiaa@gmail.com>
2022-05-03 13:46:57 +0700

    Add advanced CRUD

---
+++ advanced/assets/js/custom.js
@@ -0,0 +1,235 @@
+(function($) {
+	"use strict";
+	
+	//Color-Theme
+	$(document).on("click", "a[data-theme]", function() {
+		$("head link#theme").attr("href", $(this).data("theme"));
+		$(this).toggleClass('active').siblings().removeClass('active');
+	});
+	
+	
+	// FAQ Accordion
+	$(document).on("click", '[data-bs-toggle="collapse"]', function() {
+		$(this).toggleClass('active').siblings().removeClass('active');
+	});
+	
+	
+	// ______________Full screen
+	$(document).on("click", ".fullscreen-button", function toggleFullScreen() {
+	  $('html').addClass('fullscreen-button');
+		if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+			if (document.documentElement.requestFullScreen) {
+				document.documentElement.requestFullScreen();
+			} else if (document.documentElement.mozRequestFullScreen) {
+				document.documentElement.mozRequestFullScreen();
+			} else if (document.documentElement.webkitRequestFullScreen) {
+				document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+			} else if (document.documentElement.msRequestFullscreen) {
+				document.documentElement.msRequestFullscreen();
+			}
+		} else {
+		 $('html').removeClass('fullscreen-button');
+			if (document.cancelFullScreen) {
+				document.cancelFullScreen();
+			} else if (document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+			} else if (document.webkitCancelFullScreen) {
+				document.webkitCancelFullScreen();
+			} else if (document.msExitFullscreen) {
+				document.msExitFullscreen();
+			}
+		}
+	})
+	
+	// ______________ PAGE LOADING
+	$(window).on("load", function(e) {
+		$("#global-loader").fadeOut("slow");
+	})
+	
+	// ______________ BACK TO TOP BUTTON
+	$(window).on("scroll", function(e) {
+		if ($(this).scrollTop() > 0) {
+			$('#back-to-top').fadeIn('slow');
+		} else {
+			$('#back-to-top').fadeOut('slow');
+		}
+	});
+	$(document).on("click", "#back-to-top", function(e) {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 0);
+		return false;
+	});
+	
+	
+	// ______________ COVER IMAGE
+	$(".cover-image").each(function() {
+		var attr = $(this).attr('data-bs-image-src');
+		if (typeof attr !== typeof undefined && attr !== false) {
+			$(this).css('background', 'url(' + attr + ') center center');
+		}
+	});
+
+	// ______________Quantity Cart Increase & Descrease
+	$(function () {
+		$('.add').on('click',function(){
+			var $qty=$(this).closest('div').find('.qty');
+			var currentVal = parseInt($qty.val());
+			if (!isNaN(currentVal)) {
+				$qty.val(currentVal + 1);
+			}
+		});
+		$('.minus').on('click',function(){
+			var $qty=$(this).closest('div').find('.qty');
+			var currentVal = parseInt($qty.val());
+			if (!isNaN(currentVal) && currentVal > 0) {
+				$qty.val(currentVal - 1);
+			}
+		});
+	});
+	
+	// ______________Chart-circle
+	if ($('.chart-circle').length) {
+		$('.chart-circle').each(function() {
+			let $this = $(this);
+			$this.circleProgress({
+				fill: {
+					color: $this.attr('data-bs-color')
+				},
+				size: $this.height(),
+				startAngle: -Math.PI / 4 * 2,
+				emptyFill: 'rgba(119, 119, 142, 0.2)',
+				lineCap: 'round'
+			});
+		});
+	}
+
+	// __________MODAL
+	// showing modal with effect
+	$('.modal-effect').on('click', function(e) {
+		e.preventDefault();
+		var effect = $(this).attr('data-bs-effect');
+		$('#modaldemo8').addClass(effect);
+	});
+	// hide modal with effect
+	$('#modaldemo8').on('hidden.bs.modal', function(e) {
+		$(this).removeClass(function(index, className) {
+			return (className.match(/(^|\s)effect-\S+/g) || []).join(' ');
+		});
+	}); 
+	
+	// ______________ CARD
+	const DIV_CARD = 'div.card';
+	
+	// ___________TOOLTIP
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl)
+	})
+	
+	// __________POPOVER
+	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+	return new bootstrap.Popover(popoverTriggerEl)
+	})
+	// By default, Bootstrap doesn't auto close popover after appearing in the page
+	$(document).on('click', function (e) {
+		$('[data-toggle="popover"],[data-original-title]').each(function () {
+			//the 'is' for buttons that trigger popups
+			//the 'has' for icons within a button that triggers a popup
+			if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+				(($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
+			}
+
+		});
+	});
+	
+	// ______________ FUNCTION FOR REMOVE CARD
+	$(document).on('click', '[data-bs-toggle="card-remove"]', function(e) {
+		let $card = $(this).closest(DIV_CARD);
+		$card.remove();
+		e.preventDefault();
+		return false;
+	});
+	
+	
+	// ______________ FUNCTIONS FOR COLLAPSED CARD
+	$(document).on('click', '[data-bs-toggle="card-collapse"]', function(e) {
+		let $card = $(this).closest(DIV_CARD);
+		$card.toggleClass('card-collapsed');
+		e.preventDefault();
+		return false;
+	});
+	
+	// ______________ CARD FULL SCREEN
+	$(document).on('click', '[data-bs-toggle="card-fullscreen"]', function(e) {
+		let $card = $(this).closest(DIV_CARD);
+		$card.toggleClass('card-fullscreen').removeClass('card-collapsed');
+		e.preventDefault();
+		return false;
+	});
+	
+
+	//Input file-browser
+	$(document).on('change', '.file-browserinput', function() {
+		var input = $(this),
+		numFiles = input.get(0).files ? input.get(0).files.length : 1,
+		label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+	input.trigger('fileselect', [numFiles, label]);
+	});// We can watch for our custom `fileselect` event like this
+
+	//______File Upload
+	$('.file-browserinput').on('fileselect', function(event, numFiles, label) {
+			var input = $(this).parents('.input-group').find(':text'),
+			log = numFiles > 1 ? numFiles + ' files selected' : label;
+			if( input.length ) {
+				input.val(log);
+			} else {
+				if( log ) alert(log);
+			}
+	});
+	
+
+	
+	// ______________ SWITCHER-toggle ______________//
+	
+	$('.layout-setting').on("click", function(e) {
+		if (document) {
+			$('body').toggleClass('dark-mode');
+		} else {
+			$('body').removeClass('dark-mode');
+			$('body').addClass('light-mode');
+		}
+	});
+	
+	/*Theme Layout*/
+	// $('body').addClass('dark-mode');
+	// $('body').addClass ('light-mode');
+
+	/*Header-styles Mode*/
+	// $('body').addClass('light-header');
+	// $('body').addClass('color-header');
+	// $('body').addClass('dark-header');
+	// $('body').addClass('gradient-header');
+
+	/*Leftmenu-styles Mode*/
+	// $('body').addClass('light-menu');
+	// $('body').addClass('color-menu');
+	// $('body').addClass('dark-menu');
+	// $('body').addClass('gradient-menu');
+
+	/*Horizontal-menu Mode*/
+	// $('body').addClass('light-hormenu');
+	// $('body').addClass('color-hormenu');
+	// $('body').addClass('dark-hormenu');
+	// $('body').addClass('gradient-hormenu');
+
+	/*Layout-width Styles*/
+	// $('body').addClass('layout-fullwidth');
+	// $('body').addClass('layout-boxed');
+
+	/*Header-Position Styles*/
+	// $('body').addClass('fixed-layout');
+	// $('body').addClass('scrollable-layout');
+	
+})(jQuery);
